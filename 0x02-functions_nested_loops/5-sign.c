d31152c50eecf7329f406bd5209494caf4df4a01
Rbel14 <robelaylew2021@gmail.com>
2023-09-13 10:53:20 -0700

    vi 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,34 +1,24 @@
-#include "main.h"
-
+#include<stdio.h>
 /**
  * main - check the code.
  *
  * Return: Always 0.
  */
-int main (void)
+int print_sign(int n)
 {
-	int print_sign;
-	int r;
-
-	r = print_sign(98);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0xff);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(-1);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+	if (n > 0)
+	{
+		printf("+");
+		return (1);
+	}
+	else if ( n == 0)
+	{
+		printf("0");
+		return (0);
+	}
+	else
+	{
+		printf("-");
+		return (-1);
+	}
 }

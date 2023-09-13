bf4361f87ba1c4528eac014617b8dfc7b40f7338
Rbel14 <robelaylew2021@gmail.com>
2023-09-13 15:35:52 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,9 +14,9 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (;n <= 98; n++)
+		for (; n <= 98; n++)
 		{
-			if ( n == 98)
+			if (n == 98)
 			{
 				printf("%d", n);
 				printf("\n");
@@ -30,9 +30,9 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (;n >= 98; n--)
+		for (; n >= 98; n--)
 		{
-			if ( n == 98)
+			if (n == 98)
 			{
 				printf("%d", n);
 				printf("\n");

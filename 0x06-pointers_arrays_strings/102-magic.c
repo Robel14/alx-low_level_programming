61c24ed64d45b99110d83cc489d32d70afbcfe40
Rbel14 <robelaylew2021@gmail.com>
2023-09-21 07:39:43 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -3,6 +3,7 @@
  * main - magic file
  * Return: 0 Always
  */
+
 int main(void)
 {
 	int n;
@@ -19,7 +20,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-	;
+	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
